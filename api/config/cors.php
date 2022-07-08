76cb25832a9e29e06b8a928d2445c58214a20a0a
Alaa Aqeel <alaa.21.iraq@gmail.com>
2022-07-09 00:57:52 +0300

    update

--- api/config/cors.php
+++ api/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://menu.etana-iq.com',"http://127.0.0.1:3000",'http://localhost:3000', 'http://192.168.0.108:3000'],
+    'allowed_origins' => ['https://menu.etana-iq.com', 'http://menu.etana-iq.com',"http://127.0.0.1:3000",'http://localhost:3000', 'http://192.168.0.108:3000'],
 
     'allowed_origins_patterns' => ["*"],
 
