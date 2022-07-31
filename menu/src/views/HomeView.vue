deb5379506f63fe96e9ba090e29648554599e731
Alaa Aqeel <alaa.21.iraq@gmail.com>
2022-07-31 17:57:52 +0300

    renamed:    menu/dist/assets/404View.03ad4001.js -> menu/dist/assets/404View.2241f3fc.js 	renamed:    menu/dist/assets/AccountForm.5070a517.js -> menu/dist/assets/AccountForm.821a00cd.js 	renamed:    menu/dist/assets/Card.0a378ae9.js -> menu/dist/assets/Card.7a26f6f0.js 	renamed:    menu/dist/assets/CategoryForm.d812c7e0.js -> menu/dist/assets/CategoryForm.df107aca.js 	renamed:    menu/dist/assets/CategoryView.94df44b7.js -> menu/dist/assets/CategoryView.7456d2b1.js 	renamed:    menu/dist/assets/Chip.e6a46ec9.js -> menu/dist/assets/Chip.8d32fb2a.js 	renamed:    menu/dist/assets/Dialog.7c5779b5.js -> menu/dist/assets/Dialog.bab8a9a2.js 	renamed:    menu/dist/assets/FoodForm.89d5ee91.js -> menu/dist/assets/FoodForm.a0897625.js 	renamed:    menu/dist/assets/FoodView.08b2eb28.js -> menu/dist/assets/FoodView.5fe7f1e4.js 	renamed:    menu/dist/assets/Header.b02fef5b.js -> menu/dist/assets/Header.8bf1f286.js 	new file:   menu/dist/assets/HomeView.115a7d48.js 	deleted:    menu/dist/assets/HomeView.758fe2f3.js 	renamed:    menu/dist/assets/IndexView.61513c22.js -> menu/dist/assets/IndexView.b86b40f0.js 	renamed:    menu/dist/assets/Loading.47066203.js -> menu/dist/assets/Loading.1e1dd24a.js 	renamed:    menu/dist/assets/LoginView.0284073f.js -> menu/dist/assets/LoginView.8b7f5d0d.js 	renamed:    menu/dist/assets/MenuForm.eca41e93.js -> menu/dist/assets/MenuForm.49f07089.js 	renamed:    menu/dist/assets/MenuView.ae35c03b.js -> menu/dist/assets/MenuView.4d127b6f.js 	renamed:    menu/dist/assets/Navbar.93c932ea.js -> menu/dist/assets/Navbar.90689d42.js 	renamed:    menu/dist/assets/ProfileView.642bd7a3.js -> menu/dist/assets/ProfileView.40367811.js 	renamed:    menu/dist/assets/QRCodeView.c40c14a2.js -> menu/dist/assets/QRCodeView.6127fe8f.js 	renamed:    menu/dist/assets/iconify.d121bd07.js -> menu/dist/assets/iconify.7cbae81d.js 	renamed:    menu/dist/assets/index.5873775b.js -> menu/dist/assets/index.28087d49.js 	modified:   menu/dist/index.html 	modified:   menu/src/views/HomeView.vue

--- menu/src/views/HomeView.vue
+++ menu/src/views/HomeView.vue
@@ -70,20 +70,20 @@ const initMessanger = (pageId)=> {
 }
 
 const createMessanger = () => {
-    window.fbAsyncInit = function() {
+     window.fbAsyncInit = function() {
         FB.init({
-            xfbml            : true,
-            version          : 'API-VERSION'
+          xfbml            : true,
+          version          : 'v14.0'
         });
-    };
+      };
 
-    (function(d, s, id) {
+      (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        js.src = 'https://connect.facebook.net/ar_AR/sdk/xfbml.customerchat.js';
         fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+      }(document, 'script', 'facebook-jssdk'));
 }
 
 const getAll =  (category) => {
@@ -102,6 +102,7 @@ const getAll =  (category) => {
             router.push({name: 'notfound'})
         })
 }
+
 
 onMounted(()=> {
     getAll('')
