4bd858fb6a8ab87ae420bb125e6eb78548f5226f
Alaa Aqeel <alaa.21.iraq@gmail.com>
2023-03-10 22:46:32 +0300

    renamed:    menu/dist/assets/404View.157d9759.js -> menu/dist/assets/404View.abd62d9d.js 	renamed:    menu/dist/assets/AccountForm.8901686b.js -> menu/dist/assets/AccountForm.bdac9743.js 	renamed:    menu/dist/assets/Card.4b6ed5ce.js -> menu/dist/assets/Card.fa7fb7e9.js 	renamed:    menu/dist/assets/CategoryForm.7488cd87.js -> menu/dist/assets/CategoryForm.12f268c3.js 	renamed:    menu/dist/assets/CategoryView.4ee87221.js -> menu/dist/assets/CategoryView.0e1b0793.js 	renamed:    menu/dist/assets/Chip.ae0b4205.js -> menu/dist/assets/Chip.223f2779.js 	renamed:    menu/dist/assets/Dialog.ea8186b3.js -> menu/dist/assets/Dialog.bced9380.js 	renamed:    menu/dist/assets/FoodForm.7fb2698b.js -> menu/dist/assets/FoodForm.7dc17f7b.js 	renamed:    menu/dist/assets/FoodView.a0112df3.js -> menu/dist/assets/FoodView.8910ea97.js 	renamed:    menu/dist/assets/Header.dfd5b7d9.js -> menu/dist/assets/Header.c1f2313b.js 	new file:   menu/dist/assets/HomeView.181fe2ae.js 	deleted:    menu/dist/assets/HomeView.ef4b776c.js 	renamed:    menu/dist/assets/IndexView.a90fc190.js -> menu/dist/assets/IndexView.1e79e6be.js 	renamed:    menu/dist/assets/Loading.39438d47.js -> menu/dist/assets/Loading.6efff500.js 	renamed:    menu/dist/assets/LoginView.8d9bbdc0.js -> menu/dist/assets/LoginView.42baa252.js 	renamed:    menu/dist/assets/MenuForm.45611c67.js -> menu/dist/assets/MenuForm.872e0390.js 	renamed:    menu/dist/assets/MenuView.c2a47a30.js -> menu/dist/assets/MenuView.3cb4cff6.js 	renamed:    menu/dist/assets/Navbar.068e3243.js -> menu/dist/assets/Navbar.0395845d.js 	renamed:    menu/dist/assets/ProfileView.d832617b.js -> menu/dist/assets/ProfileView.a9be740e.js 	renamed:    menu/dist/assets/QRCodeView.a57c87e4.js -> menu/dist/assets/QRCodeView.af2cad89.js 	renamed:    menu/dist/assets/iconify.0e078bf3.js -> menu/dist/assets/iconify.d4f90947.js 	new file:   menu/dist/assets/index.22e5ee9e.css 	renamed:    menu/dist/assets/index.f755344b.js -> menu/dist/assets/index.766f46af.js 	deleted:    menu/dist/assets/index.dcb3f2fe.css 	modified:   menu/dist/index.html 	modified:   menu/src/App.vue 	modified:   menu/src/views/HomeView.vue

--- menu/src/App.vue
+++ menu/src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <div class="w-full top-0 px-2 fixed py-4 text-start ">
-        <a href="https://etana-iq.com/" class="font-bold underline underline-offset-8 hover:text-blue-900 decoration-wavy hover:decoration-solid decoration-primary">
-          ايتانا للحلول البرمجية 
-        </a>
-    </div>
+
     <div>
         <router-view></router-view>
     </div>
